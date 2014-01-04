f262779c3e605c590d867ef0c4c7d522cb97602c
Herve Peroteau <herve.peroteau@gmail.com>
2014-01-04 19:43:21 +0100

    Initial Commit

---
+++ HPBlurryStepOutAnimatedTransitioning/HPBlurryStepOutAnimatedTransitioning.h
@@ -0,0 +1,13 @@
+//
+//  HPBlurryStepOutAnimatedTransitioning.h
+//  HPBlurryStepOutAnimatedTransitioning
+//
+//  Created by Hervé PEROTEAU on 04/01/2014.
+//  Copyright (c) 2014 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPBlurryStepOutAnimatedTransitioning : NSObject
+
+@end
---
+++ HPBlurryStepOutAnimatedTransitioning/HPBlurryStepOutAnimatedTransitioning.m
@@ -0,0 +1,13 @@
+//
+//  HPBlurryStepOutAnimatedTransitioning.m
+//  HPBlurryStepOutAnimatedTransitioning
+//
+//  Created by Hervé PEROTEAU on 04/01/2014.
+//  Copyright (c) 2014 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "HPBlurryStepOutAnimatedTransitioning.h"
+
+@implementation HPBlurryStepOutAnimatedTransitioning
+
+@end
