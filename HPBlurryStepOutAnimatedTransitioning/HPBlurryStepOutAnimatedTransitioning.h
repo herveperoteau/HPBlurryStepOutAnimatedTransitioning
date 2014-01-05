4f02ced92d7fa857fe2bcbf3ac64beec9edf01a5
Herve Peroteau <herve.peroteau@gmail.com>
2014-01-05 14:14:46 +0100

    Inspired by NRBlurryStepOutAnimatedTransitioning (cocoapods)

--- HPBlurryStepOutAnimatedTransitioning/HPBlurryStepOutAnimatedTransitioning.h
+++ HPBlurryStepOutAnimatedTransitioning/HPBlurryStepOutAnimatedTransitioning.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
-@interface HPBlurryStepOutAnimatedTransitioning : NSObject
+@interface HPBlurryStepOutAnimatedTransitioning : NSObject<UIViewControllerAnimatedTransitioning>
+
+- (id) initWithPresenting: (BOOL)presenting;
+- (NSTimeInterval) transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext;
+- (void) animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext;
+
+@property (nonatomic, assign) NSTimeInterval presentationDuration;
+@property (nonatomic, assign) NSTimeInterval dismissalDuration;
+@property (nonatomic, assign) BOOL isPresenting;
+
+@property (nonatomic, strong) UIColor *endColorBackground;
 
 @end
